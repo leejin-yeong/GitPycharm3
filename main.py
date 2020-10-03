dea7ecd3663ab10d2bfd5381f22276c9819f1bbf
leejin-yeong <33474852+leejin-yeong@users.noreply.github.com>
2020-10-03 10:53:46 +0900

    Upates 2 on GitHub Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Updates on GitHub")
 
 #Upates in local master branch
 print("Hello World Upates on Local Master Branch")
+
+# Upates on main branch of GitHub
+print("Hello World Updates 2 on GitHub Only")
